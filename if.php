62e905873ba78700c4f985c106702ed3764cc0a0
Dylan Perenchio <perenchiod@gmail.com>
2015-07-13 10:35:37 -0500

    Added a few more conditionals to if.php

--- if.php
+++ if.php
@@ -6,7 +6,7 @@
 
  if ($a < $b) {
      // output the appropriate result
-     echo "$a is less than $b\n";
+     echo "$a is less than $b and $b is greater than $a\n";
  }
 
  if ($b > $a) {
@@ -44,4 +44,35 @@
      echo "$b is not identical to $c\n";
  }
 
+// Shorten the next 2 statements into an if/else
+ if ($a < $b) {
+     echo "$a is less than $b" . PHP_EOL;
+ } else {
+     echo "$b is greater than $a" . PHP_EOL;
+ }
+
+// Shorten the next 2 statements into an if/else
+ if ($b >= $c) {
+     // output the appropriate result
+     echo "$b is greater than or equal to $c" . PHP_EOL;
+ } else {
+     echo "$b is less than or equal to $c" . PHP_EOL;
+ }
+
+// combine the next 4 conditionals into
+ // one if/else/elseif block that checks in order for:
+ // identical, equal, not identical, not equal
+ if ($b == $c) {
+     echo "$b is equal to $c" . PHP_EOL;
+     if ($b === $c) {
+     echo "$b is identical to $c" . PHP_EOL;
+     }
+ } else if($b != $c) {
+     echo "$b is not equal to $c" . PHP_EOL;
+     if($b !== $c) {
+          echo "$b is not identical to $c" . PHP_EOL;
+     }
+ }
+
+ 
  ?>
