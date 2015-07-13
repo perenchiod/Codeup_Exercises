b7698db27b7255f46b6d9d3651c80a41abdc949e
Dylan Perenchio <perenchiod@gmail.com>
2015-07-13 11:38:28 -0500

    .

--- do_while.php
+++ do_while.php
@@ -1,9 +1,24 @@
 <?php 
 
-	$var = 2;
+	$var = 0;
+	$var2 = 2;
+	$var3 = 100;
+	echo "Loop 1" . PHP_EOL;
 	do {
 		echo "$var" . PHP_EOL;
-		$var = $var * $var;
-	}while($var < 1000000);
+		$var += 2;
+	}while($var < 100);
+
+	echo "Loop 2" . PHP_EOL;
+	do {
+		echo "$var2" . PHP_EOL;
+		$var2 = $var2 * $var2;
+	}while($var2 < 1000000);
+
+	echo "Loop 3" . PHP_EOL;
+	do {
+		echo "$var3" . PHP_EOL;
+		$var3 = $var3 -= 5;
+	}while($var3 >= -10);
 
 ?>
